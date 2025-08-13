d2b59b59903c75603fbed57a1d8b0b5547c1f893
Kauã Gabriell <kauagabriell64@gmail.com>
2025-08-13 15:27:31 -0300

    :tada: Finalizando projeto 01

--- projetc01/assets/script.js
+++ projetc01/assets/script.js
@@ -7,5 +7,53 @@ const produtos = [
     { id: 6, nome: 'Fone de Ouvido Bluetooth', preco: 420, categoria: 'Acessórios', emEstoque: false, tags: ['áudio', 'sem fio', 'promoção'] },
     { id: 7, nome: 'Webcam HD', preco: 80, categoria: 'Acessórios', emEstoque: true, tags: ['vídeo', 'streaming'] }
   ];
+  const divProdutos = document.querySelector('#lista-produtos-container');
+  const listaProdutos = document.querySelector('#lista-produtos');
+  const divFiltros = document.querySelector('#filtros');
 
-  
+  function renderizarProdutos(listaDeProdutos){
+    const produtosHtml = listaDeProdutos.map(function(obj){
+        const statusEstoque = obj.emEstoque ? 'Em estoque🟩' : 'Sem Estoque❌'
+
+        return `
+        <li>
+            <span>${obj.nome}</span>
+            <span>${obj.preco}</span>
+            <span>${obj.categoria}</span>
+            <span>${statusEstoque}</span>
+        </li>
+        `;
+    })
+    listaProdutos.innerHTML = produtosHtml.join('');
+  }
+
+  /**Criando Array com as Categorias para fazer os botões dinâmicos */
+  const todasAsCategorias = produtos.map(produto => produto.categoria);
+  const categoriasUnicas = [...new Set(todasAsCategorias)];
+  categoriasUnicas.unshift('Todos');
+
+
+  /**Função para Renderizar Filtros */
+  function renderizarFiltros(listaCategorias){
+    const filtros = listaCategorias.map(function(categoria){
+        return `<button>${categoria}</button>`;
+    })
+    divFiltros.innerHTML = filtros.join('');
+    
+  }
+
+
+/**Evento de Filtragem */
+divFiltros.addEventListener('click', function(e){
+    const el = e.target;
+    if(el.tagName === 'BUTTON'){
+       if(el.innerHTML === 'Todos'){
+        renderizarProdutos(produtos);
+       }else{
+        const filtrados = produtos.filter(produto => produto.categoria === el.innerHTML);
+        renderizarProdutos(filtrados);
+       }
+    }
+})
+renderizarProdutos(produtos);
+renderizarFiltros(categoriasUnicas);
